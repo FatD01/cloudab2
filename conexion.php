5b41870f53a3036d7571945e2d4628b1af4f09be
Alumno Tecsup <alumnotecsup@MacBook-Pro-de-Alumno.local>
2026-03-25 15:45:29 -0500

    secondd

--- conexion.php
+++ conexion.php
@@ -1,20 +1,15 @@
 <?php
 
-function conexion(){
+    function conexion(){
 
     $host = "host=dpg-d723dbkhg0os73am0jog-a.oregon-postgres.render.com";
     $port = "port=5432";
     $dbname = "dbname=dbtest_a1y3";
     $user = "user=dbtest_a1y3_user";
     $password = "password=vvHNmH58Ku7Er3RKtOCajujSh3yQSGBG";
-    
-    // ESTA ES LA LÍNEA QUE TE FALTA:
-    $ssl = "sslmode=require"; 
 
-    // Agregamos $ssl al final de la cadena
-    $db = pg_connect("$host $port $dbname $user $password $ssl");
+    $db = pg_connect("$host $port $dbname $user $password");
 
     return $db;
 }
-
 ?>
