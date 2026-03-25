40cbda6d11a3938c4aa2c4c75e66f68def1918fa
Alumno Tecsup <alumnotecsup@MacBook-Pro-de-Alumno.local>
2026-03-25 16:09:13 -0500

    secondddddamafd

--- conexion.php
+++ conexion.php
@@ -4,8 +4,8 @@
 
     $host = "host=dpg-d723dbkhg0os73am0jog-a.oregon-postgres.render.com";
     $port = "port=5432";
-    $dbname = "dbname=dbtest_a1y3";
-    $user = "user=dbtest_a1y3_user";
+    $dbname = "dbname=test_a1y3";
+    $user = "user=test_a1y3_user";
     $password = "password=vvHNmH58Ku7Er3RKtOCajujSh3yQSGBG";
 
     $db = pg_connect("$host $port $dbname $user $password");
